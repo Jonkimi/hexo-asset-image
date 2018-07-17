04c08770d821bfde854e0ffc37cd68bbe46096e2
ycwang <wangyongcong@gmail.com>
2018-07-17 12:19:45 +0800

    convert relative asset path to absolute path

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
 # hexo-asset-image
 
+Convert relative asset path to absolute path automatically. 
+With `post_asset_folder` set to `true`, and using relative assets path in posts and pages, asset links in markdown source file can be rendered correctly by editors.
 
-Give asset image in hexo a absolutely path automatically
+It assumes that:
+1. posts with permalink "http[s]//yoursite/:year/:month/:day/title/"
+2. pages with permalink "http[s]//yoursite/title/index.html".
 
-# Usege
+
+# Install
 
 ```shell
 npm install hexo-asset-image --save
@@ -12,13 +17,39 @@ npm install hexo-asset-image --save
 # Example
 
 ```shell
-MacGesture2-Publish
-├── apppicker.jpg
-├── logo.jpg
-└── rules.jpg
-MacGesture2-Publish.md
+source
+├── _post
+|    ├── hello-world
+|    |    ├── logo.png
+|    |    └── info.pdf
+|    └── hello-world.md
+├── about
+|    ├── index.md
+|    |    ├── avatar.png
+|    |    └── resume.pdf
+|    └── index.md
+└── assets
+     └── abs.pdf
 ```
 
 Make sure `post_asset_folder: true` in your `_config.yml`.
 
-Just use `![logo](logo.jpg)` to insert `logo.jpg`.
+In hello-world.md:
+1. `![logo](hello-world/logo.png)` 
+2. `[info](hello-world/info.pdf)`
+
+In about/index.md:
+1. `![avatar](index/avatar.png)`
+2. `[resume](index/resume.pdf)`
+
+After post render, relative asset path will be converted:
+
+In hello-world.html:
+1. `<img src="/:year/:month/:day/hello-world/logo.png" alt="logo">`
+2. `<a href="/:year/:month/:day/hello-world/info.pdf">info</a>`
+
+In about/index.html:
+1. `<img src="/about/index/avatar.png" alt="avatar">`
+2. `<a href="/about/index/resume.pdf">resume</a>`
+
+And absolute path (starts with `\`) keep unchanged.
--- index.js
+++ index.js
@@ -6,19 +6,39 @@ function getPosition(str, m, i) {
   return str.split(m, i).join(m).length;
 }
 
-hexo.extend.filter.register('after_post_render', function(data){
+function convertRelativePath(obj, tag, link, title) {
+  var src = obj.attr(tag);
+  var is_relative = false;
+  if(src.startsWith(title)) {
+    src = src.substring(title.length);
+    is_relative = true;
+  }
+  else {
+    is_relative = src.startsWith("index/");
+  }
+  if(is_relative) {
+    obj.attr(tag, '/' + link + src);
+  }
+}
+
+hexo.extend.filter.register('after_post_render', function (data) {
   var config = hexo.config;
-  if(config.post_asset_folder){
+  if (config.post_asset_folder) {
+    // By default, post permalink is of the form "http[s]//yoursite/year/month/day/title/"
+    // page permalink is of the form "http[s]//yoursite/title/index.html"
+    // Extract post permlink and title
     var link = data.permalink;
-	var beginPos = getPosition(link, '/', 3) + 1;
-	// In hexo 3.1.1, the permalink of "about" page is like ".../about/index.html".
-	var endPos = link.lastIndexOf('/') + 1;
-    link = link.substring(beginPos, endPos);
+    var beginPos = getPosition(link, '/', 3) + 1;
+    var endPos = link.lastIndexOf('/');
+    var beginPos2 = link.lastIndexOf('/', endPos - 1) + 1;
+    var title = link.substring(beginPos2, endPos + 1);
+    link = link.substring(beginPos, endPos + 1);
+    // console.log("DEBUG:", data.permalink, "|", link, "|", title);
 
     var toprocess = ['excerpt', 'more', 'content'];
-    for(var i = 0; i < toprocess.length; i++){
+    for (var i = 0; i < toprocess.length; i++) {
       var key = toprocess[i];
- 
+
       var $ = cheerio.load(data[key], {
         ignoreWhitespace: false,
         xmlMode: false,
@@ -26,30 +46,11 @@ hexo.extend.filter.register('after_post_render', function(data){
         decodeEntities: false
       });
 
-      $('img').each(function(){
-		if ($(this).attr('src')){
-			// For windows style path, we replace '\' to '/'.
-			var src = $(this).attr('src').replace('\\', '/');
-			if(!/http[s]*.*|\/\/.*/.test(src) &&
-			   !/^\s*\//.test(src)) {
-			  // For "about" page, the first part of "src" can't be removed.
-			  // In addition, to support multi-level local directory.
-			  var linkArray = link.split('/').filter(function(elem){
-				return elem != '';
-			  });
-			  var srcArray = src.split('/').filter(function(elem){
-				return elem != '' && elem != '.';
-			  });
-			  if(srcArray.length > 1)
-				srcArray.shift();
-			  src = srcArray.join('/');
-			  $(this).attr('src', config.root + link + src);
-			  console.info&&console.info("update link as:-->"+config.root + link + src);
-			}
-		}else{
-			console.info&&console.info("no src attr, skipped...");
-			console.info&&console.info($(this));
-		}
+      $('img').each(function () {
+        convertRelativePath($(this), 'src', link, title);
+      });
+      $('a').each(function () {
+        convertRelativePath($(this), 'href', link, title);
       });
       data[key] = $.html();
     }
